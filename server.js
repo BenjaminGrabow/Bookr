dfe8730b4ed80f37f55425c1e136bc14d45a5ea3
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-29 10:52:32 +0200

    Feature: added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const configureRoutes = require('../config/routes.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+configureRoutes(server);
+
+module.exports = server;
