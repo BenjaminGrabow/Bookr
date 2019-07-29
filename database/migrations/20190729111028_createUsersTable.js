1510179a317588f2301064a2a2d6fb8f5900b004
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-29 11:20:45 +0200

    Feature: added users table

--- database/migrations/20190729111028_createUsersTable.js
+++ database/migrations/20190729111028_createUsersTable.js
@@ -1,8 +1,16 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', users => {
+    users.increments();
 
-exports.up = function(knex, Promise) {
-  
+    users
+      .string('username', 50)
+      .notNullable()
+      .unique();
+    users.string('password', 50)
+    .notNullable();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('users');
 };
