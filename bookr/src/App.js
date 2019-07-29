e07f42c461fcf715cd19e1702a29de61ff4d02c5
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-29 16:18:04 +0200

    Feature: added LogAndReg to App.js

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import LogAndReg from './Components/Login/LogAndRegi';
+import PrivateRoute from './Components/Login/PrivateRoute';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Route path="/" component={LogAndReg} />
     </div>
   );
 }
