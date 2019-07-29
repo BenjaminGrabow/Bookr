735a959fe85b198cb35bbc02c701c50016751563
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-29 20:02:27 +0200

    Feature: changed logic for login and created actions for register, login and fetching books

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import LogAndReg from './Components/Login/LogAndRegi';
 import PrivateRoute from './Components/Login/PrivateRoute';
+import AllBooks from './Components/Bookr/AllBooks/AllBooks';
 import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={LogAndReg} />
+      <Route exact path="/log_reg" component={LogAndReg} />
+      <PrivateRoute path="/bookr" component={AllBooks} />
     </div>
   );
 }
