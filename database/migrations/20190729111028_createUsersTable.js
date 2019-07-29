e80eb9dae47b197f01949d1575972d67481c60a7
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-29 12:21:03 +0200

    Feature: added books and reviews table to migration

--- database/migrations/20190729111028_createUsersTable.js
+++ database/migrations/20190729111028_createUsersTable.js
@@ -1,4 +1,4 @@
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema.createTable('users', users => {
     users.increments();
     users
@@ -6,10 +6,31 @@ exports.up = function(knex) {
       .notNullable()
       .unique();
     users.string('password', 50)
-    .notNullable();
-  });
+      .notNullable();
+  })
+    .createTable('books', books => {
+      books.increments();
+      books.text('title', 50).notNullable();
+      books.text('author', 50).notNullable();
+      books.text('publisher', 50).notNullable();
+      books.text('description', 1000);
+    })
+    .createTable('reviews', reviews => {
+      reviews.increments();
+      reviews.text('reviewer', 50).notNullable();
+      reviews.text('review', 500).notNullable();
+      reviews
+        .integer("book_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("books");
+    });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
+exports.down = function (knex, Promise) {
+  return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('books')
+    .dropTableIfExists('reviews');
 };
