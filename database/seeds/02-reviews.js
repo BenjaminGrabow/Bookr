a29e908d6af47f808b05e9d3a1ea30c873a1b7f1
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-29 14:16:01 +0200

    Feature: added data to seed file for reviews

--- database/seeds/02-reviews.js
+++ database/seeds/02-reviews.js
@@ -1,13 +1,31 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('reviews').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('reviews').insert([
+        {id: 1, reviewer: 'Ryan',
+      review: 'Awesome book !',
+      book_id: 1
+    },
+        {id: 2, reviewer: 'Peter',
+      review: 'Good read, was fully worth my time.',
+      book_id: 1
+    },
+        {id: 3, reviewer: 'Lisa',
+      review: 'Fascinating book !',
+      book_id: 1
+    },     
+    {id: 4, reviewer: 'Ben',
+    review: 'I loved to spend time with this book.',
+    book_id: 2
+  },
+      {id: 5, reviewer: 'Karim',
+    review: 'Awesome book !',
+    book_id: 3
+  },
+      {id: 6, reviewer: 'Daniel',
+    review: 'Super interesting story.',
+    book_id: 3
+  }
       ]);
     });
 };
