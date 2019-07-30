9575389083944c9fbe7d5998bd1a093087b7c2c2
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-30 19:58:27 +0200

    Feature: added 2 new books to seed data in books table

--- database/seeds/01-books.js
+++ database/seeds/01-books.js
@@ -34,6 +34,38 @@ exports.seed = function (knex, Promise) {
               to millions of readers the world over.`,
           photo: 'https://jamesclear.com/wp-content/uploads/2017/04/The-Kite-Runner-by-Khaled-Hosseini-444x700.jpg',
           price: 20
+        },
+        {
+          id: 4, title: `Thinking, Fast and Slow`,
+          publisher: 'Amazon',
+          author: 'Daniel Kahneman',
+          description: `The phenomenal international bestseller - 2 million copies sold - that will change the way you make decisions
+
+          'A lifetime's worth of wisdom' Steven D. Levitt, co-author of Freakonomics
+          'There have been many good books on human rationality and irrationality, but only one masterpiece.
+           That masterpiece is Thinking, Fast and Slow' Financial Times`,
+          photo: 'https://images-na.ssl-images-amazon.com/images/I/41jZIThj0aL._SX324_BO1,204,203,200_.jpg',
+          price: 10.19
+        },
+        {
+          id: 5, title: 'The Power of Habit: Why We Do What We Do, and How to Change',
+          publisher: 'Amazon',
+          author: 'Charles Duhigg',
+          description: `We can always change. In The Power of Habit, award-winning New York Times business reporter Charles 
+          Duhigg translates cutting-edge behavioural science into practical self-improvement action, distilling advanced neuroscience
+           into fascinating narratives of transformation. `,
+          photo: 'https://images-na.ssl-images-amazon.com/images/I/41owDNwBMNL._SX324_BO1,204,203,200_.jpg',
+          price: 12.99
+        },
+        {
+          id: 6,
+          title: 'The Kite Runner',
+          publisher: 'Amazon',
+          author: 'Khaled Hosseini',
+          description: `The #1 New York Times bestselling debut novel that introduced Khaled Hosseini 
+              to millions of readers the world over.`,
+          photo: 'https://jamesclear.com/wp-content/uploads/2017/04/The-Kite-Runner-by-Khaled-Hosseini-444x700.jpg',
+          price: 20
         }
       ]);
     });
