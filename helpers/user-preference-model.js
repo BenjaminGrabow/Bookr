7557c22069a50af9f07913ff3424336b0373a45a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-30 21:55:48 +0200

    Feature: added user-preference-model

---
+++ helpers/user-preference-model.js
@@ -0,0 +1,36 @@
+const db = require("../database/dbConfig.js");
+
+module.exports = {
+  addUserPreference,
+  findUserPreferences,
+  findBy,
+  findById,
+  updateUserPreferenceById,
+};
+
+function findUserPreferences() {
+  return db("user_preference").select("id", "first_name");
+}
+
+function findBy(filter) {
+  return db("user_preference").where(filter);
+}
+
+async function addUserPreference(userPreference) {
+  const [id] = await db("user_preference").insert(userPreference);
+
+  return findById(id);
+}
+
+function findById(id) {
+  return db("users")
+    .select("id", "username")
+    .where({ id })
+    .first();
+}
+
+const updateUserPreferenceById = ({ username, password, photo, first_name, last_name }, id) => {
+  return db('users')
+    .where({ id })
+    .update({ username, password, photo, first_name, last_name });
+};
