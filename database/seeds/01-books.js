b2a1da43df54fad447c41ceab19b29fe862b76d3
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-30 13:25:42 +0200

    Feature: changed prices in seed file for books

--- database/seeds/01-books.js
+++ database/seeds/01-books.js
@@ -23,7 +23,7 @@ exports.seed = function (knex, Promise) {
             look at the extraordinary life of one of Silicon Valley’s most exciting, unpredictable, and ambitious
              entrepreneurs—a real-life Tony Stark—and a fascinating exploration of the renewal of American invention`,
           photo: 'https://images-na.ssl-images-amazon.com/images/I/51uWemcw0gL._SX329_BO1,204,203,200_.jpg',
-          price: 30
+          price: 55
         },
         {
           id: 3,
@@ -33,7 +33,7 @@ exports.seed = function (knex, Promise) {
           description: `The #1 New York Times bestselling debut novel that introduced Khaled Hosseini 
               to millions of readers the world over.`,
           photo: 'https://jamesclear.com/wp-content/uploads/2017/04/The-Kite-Runner-by-Khaled-Hosseini-444x700.jpg',
-          price: 30
+          price: 20
         }
       ]);
     });
