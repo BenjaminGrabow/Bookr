9780de217a741900fde262bd423bf92614efaeaa
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-30 16:27:59 +0200

    Feature: deleted payment import in app.js

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import LogAndReg from './Components/Login/LogAndRegi';
 import PrivateRoute from './Components/Login/PrivateRoute';
 import Bookr from './Components/Bookr/Bookr';
-import Payment from './Components/Bookr/Payment';
 import { Route } from 'react-router-dom';
 import './App.css';
 
@@ -11,7 +10,6 @@ function App() {
     <div className="App">
       <Route exact path="/log_reg" component={LogAndReg} />
       <PrivateRoute exact path="/bookr" component={Bookr} />
-      <Route path="/payment" component={Payment} />
     </div>
   );
 }
