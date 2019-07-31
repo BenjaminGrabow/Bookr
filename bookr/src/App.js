0cae1c4f5da36e2b6d80bb35219194c78aa0cd3b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-31 17:30:57 +0200

    Feature: import slider in App.js

--- bookr/src/App.js
+++ bookr/src/App.js
@@ -3,6 +3,7 @@ import LogAndReg from './Components/Login/LogAndRegi';
 import PrivateRoute from './Components/Login/PrivateRoute';
 import Bookr from './Components/Bookr/Bookr';
 import User from './Components/Bookr/User/User';
+import SlideModus from './Components/Bookr/SlideModus/SlideModus';
 import { Route } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Route exact path="/log_reg" component={LogAndReg} />
       <PrivateRoute exact path="/bookr" component={Bookr} />
       <Route path="/user" component={User} />
+      <Route path="/slider" component={SlideModus} />
     </div>
   );
 }
