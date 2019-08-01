dd0d8f8667a6da5682a971777c3767d9b887f2fd
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-01 22:48:46 +0200

    Feature: added all inputs with all attributes in navigation

--- bookr/src/Components/Bookr/Navigation/Navigation.js
+++ bookr/src/Components/Bookr/Navigation/Navigation.js
@@ -3,47 +3,79 @@ import React from 'react';
 class Navigation extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className="navigation">
-       <input 
-       type="checkbox"
-        className="navigation__checkbox"
-      id="navi-toggle"/>
+        <input
+          type="checkbox"
+          className="navigation__checkbox"
+          id="navi-toggle" />
 
-      <label className="navigation__button" htmlFor="navi-toggle" >MENU</label>
+        <label className="navigation__button" htmlFor="navi-toggle" >MENU</label>
 
-      <div className="navigation__background">&nbsp;</div>
+        <div className="navigation__background">&nbsp;</div>
 
-      <nav className="navigation__nav">
+        <nav className="navigation__nav">
 
-        <ul className="navigation__list">
-          <li className="navigation__item">
-            <input
-             className="navigation__input"/></li>
+          <ul className="navigation__list">
+            <li className="navigation__item">
+              <input
+                type="text"
+                value={this.state.title}
+                onChange={this.changeHandler}
+                name="title"
+                className="navigation__input" /></li>
 
-<li className="navigation__item">
-            <input
-             className="navigation__input"/></li>
+            <li className="navigation__item">
+              <input
+                type="text"
+                value={this.state.publisher}
+                onChange={this.changeHandler}
+                name="publisher"
+                className="navigation__input" /></li>
 
-<li className="navigation__item">
-            <input
-             className="navigation__input"/></li>
+            <li className="navigation__item">
+              <input
+                type="text"
+                value={this.state.author}
+                onChange={this.changeHandler}
+                name="author"
+                className="navigation__input" /></li>
 
-<li className="navigation__item">
-            <input
-             className="navigation__input"/></li>
+            <li className="navigation__item">
+              <input
+                type="text"
+                value={this.state.description}
+                onChange={this.changeHandler}
+                name="description"
+                className="navigation__input" /></li>
 
-<li className="navigation__item">
-            <input
-             className="navigation__input"/></li>
-        </ul>
-      </nav>
+            <li className="navigation__item">
+              <input
+                type="text"
+                value={this.state.photo}
+                onChange={this.changeHandler}
+                name="photo"
+                className="navigation__input" /></li>
+
+            <li className="navigation__item">
+              <input
+                type="number"
+                value={this.state.price}
+                onChange={this.changeHandler}
+                name="price"
+                className="navigation__input" /></li>
+
+  
+
+
+          </ul>
+        </nav>
       </div>
-     );
+    );
   }
 }
- 
+
 export default Navigation;
